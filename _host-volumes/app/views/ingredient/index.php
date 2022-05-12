f5536185a078a519c2f6532cb169d9f657fd0c63
Maksim Pevnev <max.stak.job@gmail.com>
2022-05-12 11:21:03 +0400

    crud ingredient

---
+++ _host-volumes/app/views/ingredient/index.php
@@ -0,0 +1,41 @@
+<?php
+
+use app\models\Ingredient;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Ingredients';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="ingredient-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Ingredient', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Ingredient $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
