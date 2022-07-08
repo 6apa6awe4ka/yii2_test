66e02ec4df04d966107e57b6971e954de253bfd3
Maksim Pevnev <max.stak.job@gmail.com>
2022-07-08 09:46:41 +0400

    thin

--- _host-volumes/app/models/Dish.php
+++ _host-volumes/app/models/Dish.php
@@ -53,4 +53,28 @@ class Dish extends \yii\db\ActiveRecord
     {
         return $this->hasMany(Ingredient::className(), ['id' => 'ingredient_id'])->viaTable('dish_ingredient', ['dish_id' => 'id']);
     }
+
+    /**
+     * @param int[] $ingredientIds IDS
+     * @return \yii\db\ActiveQuery
+     */
+    public static function searchQuery($ingredientIds)
+    {
+        $subQuery = DishIngredient::find()
+            ->select(['dish_id', 'COUNT(*) as cnt'])
+            ->groupBy('dish_id')
+            ->orderBy(['cnt' => SORT_DESC])
+            ->having(['>', 'COUNT(*)', 1])
+            ->where(['in', 'ingredient_id', $ingredientIds])
+        ;
+
+        $query = (new \yii\db\Query())
+            ->select(['dish.id', 'dish.name'])
+            ->from($subQuery)
+            ->leftJoin('dish', 'dish.id = dish_id')
+            ->orderBy(['cnt' => SORT_DESC])
+        ;
+        
+        return $query;
+    }
 }
